1e036a4bf99a7d570e13becbe782a5810c0107b3
Katherine Lawrence <jsgoat@Katherines-Air.fios-router.home>
2022-03-07 20:25:36 -0500

    separated function concerns in js file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const quote = document.getElementById('quote')
-const dice = document.getElementById('button').addEventListener('click', e =>{
-    const api = 'https://api.adviceslip.com/advice'
-    async function getQuote(){
-        let adviceSlip = ''
-        let response = await fetch(api)
-        let {slip} = await response.json()
-        console.log(slip.advice)
-        quote.innerText = slip.advice
-    }
-    getQuote()
-})
+const quoteNum = document.getElementById('quote-number')
+const api = 'https://api.adviceslip.com/advice'
+const dice = document.getElementById('button').addEventListener('click', getAdvice)
+async function getAdvice(){
+    let response = await fetch(api)
+    let {slip} = await response.json()
+    console.log(slip)
+    displayAdvice(slip)
+}
+function displayAdvice(data){
+    quote.textContent = data.advice
+    quoteNum.textContent = `Quote #0${data.id}`
+}
