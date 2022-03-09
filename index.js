be7af9fed7eca92999f360823275f57d7a3df84d
Katherine Lawrence <jsgoat@Katherines-Air.fios-router.home>
2022-03-09 10:05:56 -0500

    took away unnecessary lines of code and removed prefilled html elements

--- index.js
+++ index.js
@@ -7,34 +7,25 @@ const diceFace = document.querySelector('.button')
 let slipID = 0
 let wait = false
 const dice = document.getElementById('button').addEventListener('click', (e)=>{
-    e.preventDefault()
     if (wait) return
     diceFace.classList.add('loading')
     getAdvice()
 })
-
-// GET AN ADVICE FROM THE ADVICE API THEN CALL THE DISPLAY FUNC WITH THE ADVICE DATA AS THE PARAMETER
+// GET AN ADVICE FROM THE ADVICE API
 async function getAdvice(){
-    let newSlipID = 0
     wait = true
     let response = await fetch(api)
     let {slip} = await response.json()
-    newSlipID = slip.id
-    if (slipID === newSlipID)
-        getAdvice()
-    else{
-        
-        displayAdvice(slip)   
-    }
+    slipID === slip.id ? getAdvice() : displayAdvice(slip)
 }
-
 // DISPLAY THE NEW ADVICE AND ADVICE ID
 function displayAdvice(data){
     quote.textContent = data.advice
-    quoteNum.textContent = `Quote #0${data.id}`
+    quoteNum.textContent = `Advice #0${data.id}`
     setTimeout(()=>{
         diceFace.classList.remove('loading')
         wait = false
         slipID = data.id
-    }, 200)
+    }, 100)
 }
+getAdvice()
